c90e6574fa4c02ad9bcb54e4f6bb7362f2660e49
Aswin-S-T <aswinswebart15@gmail.com>
2024-01-25 00:04:39 -0500

    header and banner page responsive done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,16 +3,18 @@ import Header from "./Components/Header/Header";
 import Banner from "./Components/Banner/Banner";
 import About from "./Pages/About/About";
 import Portfolio from "./Pages/Portfolio/Portfolio";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomeScreen from "./Screens/HomeScreen";
+import GalleryScreen from "./Screens/GalleryScreen";
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Banner />
-      <About />
-
-      <Portfolio />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomeScreen />} />
+        <Route path="/gallery" element={<GalleryScreen />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
